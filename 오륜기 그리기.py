3647a2db79c4d48b47c92456e70316d488fbc9fa
Gongmijeong <60841332+Gongmijeong@users.noreply.github.com>
2020-04-08 05:41:56 +0900

    python
    first comment

---
+++ 오륜기 그리기.py
@@ -0,0 +1,25 @@
+import turtle
+t=turtle.Pen()    #펜 생성
+t.shape("turtle") #거북이 모양으로 변경
+t.up()            #펜 올리기
+t.goto(-190,30)   #좌표이동
+t.down()          #펜 내리기
+t.circle(100)     #원 그리기
+t.up()
+t.goto(0,30)
+t.down()
+t.circle(100)
+t.up()
+t.goto(190,30)
+t.down()
+t.circle(100)
+t.up()
+t.goto(-100,-100)
+t.down()
+t.circle(100)
+t.up()
+t.goto(100,-100)
+t.down()
+t.circle(100)
+
+
